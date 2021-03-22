b32d3aa043e2dbb23a61bd8dfeb7e00693cfe444
yusuf emrah ilik <63379237+emrahilik@users.noreply.github.com>
2021-03-22 01:33:58 -0500

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3"{
-        bucket = "terraform-backend-virginia-yusufemrahilik"
-        key = "dev/infra/tfstate"
-        region = "us-east-1"
-        #dynamodb_table = "prod-stage"
-
-    }
-}
